70c271e820d3a9c4cec8778054517272274a8eb2
Anna Pawl <annampawl@gmail.com>
2018-10-14 13:02:23 -0700

    added 'facilities' and additional power level functions for 'power user stories'

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ buyGrandma.addEventListener("click", function() {
     //update price
     grandmaPriceAmount = Math.floor(grandmaPriceAmount * 1.33);
     //update grandma power
-    grandmaPower += 10;
+    grandmaPower += 10 + Math.floor(grandmaLevelNumber * 1.33);
     //turn autoGrandma on!
     autoGrandma = true
     autoGrandmaStart();
@@ -117,4 +117,61 @@ let refreshGrandma = function() {
   grandmaLevel.innerHTML = grandmaLevelNumber
   grandmaPrice.innerHTML = grandmaPriceAmount;
   grandmaMultiple.innerHTML = grandmaPower - 10;
+}
+
+
+/********************************
+
+          Facilities
+
+********************************/
+
+//set default values
+let facilityAuto = false;
+let facilityPower = 2000;
+let facilityPriceAmount = 100000;
+let facilityLevelNumber = 0;
+
+//declare DOM variables
+let buyFacility = document.getElementById('buy-facility');
+let facilityPrice = document.getElementById('facility-price');
+let facilityLevel = document.getElementById('facility-level');
+let facilityMultiple = document.getElementById('facility-multiple');
+
+//buy a facility
+buyFacility.addEventListener("click", function() {
+//set autoLoop to false
+facilityAuto = false;
+//make sure we have enough cookies
+if (cookieCount >= facilityPriceAmount) {
+cookieCount -= facilityPriceAmount;
+ refreshCookieCount()
+//upgrade power level
+facilityLevelNumber += 1;
+//update price
+facilityPriceAmount = Math.floor(facilityPriceAmount * 1.33);
+//update facility power
+ facilityPower += 600 + Math.floor(facilityLevelNumber * 1.33);
+//turn autoFacility on!
+facilityAuto = true
+autoFacilityStart();
+//refresh shop item
+refreshFacility();
+
+  }
+})
+
+// Game loop
+let autoGrandmaStart = function () {
+    let grandmaInt = window.setInterval(function(){
+        cookieCount += grandmaPower;
+        refreshCookieCount();
+    }, 1000);
+}
+
+//Refresh Shop
+let refreshFacility = function() {
+  facilityLevel.innerHTML = facilityLevelNumber
+  facilityPrice.innerHTML = facilityPriceAmount;
+  facilityMultiple.innerHTML = facilityPower - 600;
 }
