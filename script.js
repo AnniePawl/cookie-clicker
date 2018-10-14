d1b09dcdf82c32f50cfe0e5629c0ff912d89c82d
Anna Pawl <annampawl@gmail.com>
2018-10-13 19:34:19 -0700

    added 'shop', funtioning 'upgrade click power' button

--- script.js
+++ script.js
@@ -17,19 +17,49 @@ let buyClickPower = document.getElementById('buy-click-power');
 let clickPowerPrice = document.getElementById('click-power-price');
 let clickPowerLevel = document.getElementById('click-power-level');
 let clickPowerMultiple = document.getElementById('click-power-multiple');
+cookieCounter.innerHTML = cookieCount;
 
 
-//Event Listener -- everytime button is clicked, cookies increase by the value of their clickPower.
-cookieClicker.addEventListener("click", function() {
-  cookieCount = cookieCount + clickPower;
-  cookieCounter.innerHTML = cookieCount;
+cookieClicker.addEventListener("click", function() { cookieCount = cookieCount + clickPower;
+// update cookieCounter
+refreshCookieCount()
 })
+
+if (cookieCount >= clickPowerPriceAmount) {
+  //subtract cookies from the price of the item
+  cookieCount -= clickPowerPriceAmount;
+  refreshCookieCount()
+}
 
 //Event Listener -- Buy Click Power
-buyClickPower.addEventListener("click", function() {
-  if (cookieCount >= clickPowerPriceAmount) {
-    console.log("Item succesfully Bought");
-  } else {
-    console.log("You don't have enough cookies!");
+buyClickPower.addEventListener('click', function () {
+    if (cookieCount >= clickPowerPriceAmount) {
+        console.log("item succesfully Bought");
+        // subtract cookies from the price of the item
+        cookieCount -= clickPowerPriceAmount;
+        // update cookieCounter
+        refreshCookieCount()
+        // Upgrade power Level
+        clickPowerLevelNumber += 1;
+        // Update click price
+        clickPowerPriceAmount = Math.floor(clickPowerPriceAmount * 1.33);
+        // Update click power
+        clickPower += 1 * Math.floor(clickPowerLevelNumber * 1.05);
+        // refresh shop  items
+        refreshPowerClick()
+    } else {
+        console.log("you don't have enough cookies!");
   }
 })
+
+//cookieCount Function
+let refreshCookieCount = function() {
+  cookieCounter.innerHTML = cookieCount;
+}
+
+//Refresh Shop Function
+let refreshPowerClick = function() {
+  clickPowerLevel.innterHTML = clickPowerLevelNumber
+  clickPowerPrice.innerHTML = clickPowerPriceAmount;
+  clickPowerMultiple.innerHTML = clickPower;
+}
